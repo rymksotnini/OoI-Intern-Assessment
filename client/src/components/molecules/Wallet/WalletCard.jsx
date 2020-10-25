cdfd789494eb3619426be066cbdbcc1c99891850
rymksontini <rymksontini@gmail.com>
2020-10-25 07:48:43 -0400

    formating values and using icons

--- client/src/components/molecules/Wallet/WalletCard.jsx
+++ client/src/components/molecules/Wallet/WalletCard.jsx
@@ -6,6 +6,15 @@ import Block from "../../atoms/Block";
 import Outlined from "../../atoms/Button/Outlined";
 import getWallets from "../../../actions/getWallets";
 import CardBalance from "../../atoms/Text/CardBalance";
+import Contained from "../../atoms/Button/Contained";
+import EditIcon from "../../atoms/Icons/EditIcon";
+import CancelIcons from "../../atoms/Icons/CancelIcons";
+import WalletsIcon from "../../atoms/Icons/WalletsIcon";
+import EnvelopeIcon from "../../atoms/Icons/EnvelopeIcon";
+import PriceIcon from "../../atoms/Icons/PriceIcon";
+import USDIcon from "../../atoms/Icons/USDIcon";
+import {cryptoFormatter, usdFormatter} from "../../../util";
+import {colors} from "@material-ui/core";
 
 
 export default class WalletCard extends Component  {
@@ -42,9 +51,41 @@ export default class WalletCard extends Component  {
                         if (this.state.wallets[index].currency === "Ethereum"){
                             return (
                                 <Card key={index}>
-                                    <CardTitle >{wallet.name}</CardTitle>
-                                    <CardAddress >{wallet.address}</CardAddress>
-                                    <CardBalance>{wallet.balance}</CardBalance>
+                                    <CardTitle>
+                                        <WalletsIcon/>
+                                    {wallet.name}
+                                    </CardTitle>
+
+                                    <CardAddress >
+                                        <EnvelopeIcon/>
+                                        {wallet.address}
+                                    </CardAddress>
+                                    <br/>
+                                    <CardBalance>
+                                        <PriceIcon/>
+                                        <span style={{marginRight: "15px"}}>Balance(Eth) </span>
+                                        {cryptoFormatter(wallet.balance)}
+                                    </CardBalance>
+                                    <br/>
+                                    <br/>
+                                    <CardBalance>
+                                        <USDIcon/>
+                                        <span style={{marginRight: "15px"}}>Balance(USD) </span>
+                                        {usdFormatter.format(wallet.balance * this.props.rate)}
+                                    </CardBalance>
+                                    <br/>
+                                    <br/>
+                                    <div>
+                                        <Outlined>
+                                            <EditIcon/>
+                                            Edit
+                                        </Outlined>
+
+                                        <Contained style={{backgroundColor: "#9f1a1f",marginLeft:"15px"}}>
+                                            <CancelIcons/>
+                                            Delete
+                                        </Contained>
+                                    </div>
                                 </Card>
 
                             )
@@ -60,12 +101,46 @@ export default class WalletCard extends Component  {
 
                 {
                     this.state.wallets.map((wallet,index) => {
+                        console.log(this.state.wallets);
+                        console.log(this.state.wallets[index].currency);
                         if (this.state.wallets[index].currency === "Bitcoin"){
                             return (
                                 <Card key={index}>
-                                    <CardTitle >{wallet.name}</CardTitle>
-                                    <CardAddress >{wallet.address}</CardAddress>
-                                    <CardBalance>{wallet.balance}</CardBalance>
+                                    <CardTitle>
+                                        <WalletsIcon/>
+                                        {wallet.name}
+                                    </CardTitle>
+
+                                    <CardAddress >
+                                        <EnvelopeIcon/>
+                                        {wallet.address}
+                                    </CardAddress>
+                                    <br/>
+                                    <CardBalance>
+                                        <PriceIcon/>
+                                        <span style={{marginRight: "15px"}}>Balance(Bitcoin) </span>
+                                        {cryptoFormatter(wallet.balance)}
+                                    </CardBalance>
+                                    <br/>
+                                    <br/>
+                                    <CardBalance>
+                                        <USDIcon/>
+                                        <span style={{marginRight: "15px"}}>Balance(USD) </span>
+                                        {usdFormatter.format(wallet.balance * this.props.rate)}
+                                    </CardBalance>
+                                    <br/>
+                                    <br/>
+                                    <div>
+                                        <Outlined>
+                                            <EditIcon/>
+                                            Edit
+                                        </Outlined>
+
+                                        <Contained style={{backgroundColor: "#9f1a1f",marginLeft:"15px"}}>
+                                            <CancelIcons/>
+                                            Delete
+                                        </Contained>
+                                    </div>
                                 </Card>
 
                             )
