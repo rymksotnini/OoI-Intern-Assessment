94f32b1a3110411db8497140ab9966a285da14d6
rymksontini <rymksontini@gmail.com>
2020-10-24 20:13:16 -0400

    static front changes

--- client/src/components/molecules/Wallet/WalletCard.jsx
+++ client/src/components/molecules/Wallet/WalletCard.jsx
@@ -1,1 +1,24 @@
-a
+import React from "react";
+import CardTitle from "../../atoms/Text/CardTitle";
+import CardAddress from "../../atoms/Text/CardAddress";
+import WalletCard from "../../atoms/Card";
+import Outlined from "../../atoms/Button/Outlined";
+
+export default function () {
+    return (
+	<React.Fragment>
+            <WalletCard>
+                <CardTitle>wallet 1</CardTitle>
+                <CardAddress>0x000000000</CardAddress>
+		<Outlined>delete</Outlined>
+            </WalletCard>
+
+	    <WalletCard>
+                <CardTitle>wallet 2</CardTitle>
+                <CardAddress>0x000000001</CardAddress>
+		<Outlined>delete</Outlined>
+            </WalletCard>
+	</React.Fragment>
+    );
+}
+
--- client/src/components/templates/Wallets.jsx
+++ client/src/components/templates/Wallets.jsx
@@ -2,6 +2,9 @@ import React from "react";
 import { makeStyles } from "@material-ui/core/styles";
 import Grid from "@material-ui/core/Grid";
 import PriceInfo from "../molecules/Info/PriceInfo";
+import Contained from "../atoms/Button/Contained";
+import Outlined from "../atoms/Button/Outlined";
+import WalletCard from "../molecules/Wallet/WalletCard";
 
 const useStyles = makeStyles((theme) => ({
   root: {
@@ -15,9 +18,12 @@ export default function ({ wallets, rate }) {
     <div className={classes.root}>
       <Grid container>
         <Grid item xs={12}>
-          <PriceInfo />
+          <WalletCard>
+          </WalletCard>
+	  <PriceInfo/>
         </Grid>
       </Grid>
     </div>
   );
 }
+
